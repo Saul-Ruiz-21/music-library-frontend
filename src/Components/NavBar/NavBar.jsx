c347369b4e6b8ee778e703cda73fe07d3f6ddd49
Saul Ruiz <saulruiz730@gmail.com>
2022-11-03 13:46:07 -0500

    created NavBar and other componets

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import './NavBar.css'
+
+const NavBar = (props) => {
+    return (
+        <div className='Nav'>
+            <h1>
+                MeTune Music
+            </h1>
+        </div>
+    )
+}
+
+
+export default NavBar;
