0b0c0414242871678662cb6ef60360419ff9bf27
Saul Ruiz <saulruiz730@gmail.com>
2022-11-03 13:58:24 -0500

    installed axios and created useEffect function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import NavBar from "./Components/NavBar/NavBar";
 
 
 function App() {
 
-  const [entries, setEntries] = useState([])
+  const [songs, setSongs] = useState([{}])
+
+  useEffect(() => {
+    getAllSongs();
+  }, [])
+
+  async function getAllSongs(){
+    let response = await axios.get('http://127.0.0.1:8000/api/music/');
+    setSongs(response.data)
+  }
 
   return (
     <div className="App">
